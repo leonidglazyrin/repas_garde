58a8a0d4fcabe74330fe0fcfd89022057e8ecdcf
Leonid Glazyrin <83522145+leonidglazyrin@users.noreply.github.com>
2026-09-06 12:58:13 -0400

    Mount realtime discovery dishes section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import DiscoverDishes from "./DiscoverDishes.jsx";
 import "./autosaveRealtime.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <DiscoverDishes />
   </React.StrictMode>
 );
