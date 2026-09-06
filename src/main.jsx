c65cc31d15c040c663636013ad2915c62eb49045
Leonid Glazyrin <83522145+leonidglazyrin@users.noreply.github.com>
2026-09-06 14:23:25 -0400

    Enable grocery category grouping

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Accompaniments from "./Accompaniments.jsx";
 import "./autosaveRealtime.js";
 import "./libraryDisclosure.js";
 import "./groceryGrouping.js";
+import "./groceryCategories.js";
 
 const DiscoverDishes = React.lazy(() => import("./DiscoverDishes.jsx"));
 
